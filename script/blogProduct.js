36b28235afdd69d0daa3041dc029141898ba20e9
shinesha_W <shinesha1112@gmail.com>
2024-12-10 23:53:22 +0530

    blog products

---
+++ script/blogProduct.js
@@ -0,0 +1,36 @@
+export const blogPoducts=[{
+    img:"../img/blog/bl2.jpg",
+    name:"Bold and Beautiful",
+    des:"Elevate your wardrobe with trendy styles that blend modern designs with classic sophistication. Perfect for making a statement wherever you go.",
+    number:"13/01"
+},
+{
+    img:"../img/blog/bl3.jpg",
+    name:"Effortless Elegance",
+    des:" Chic outfits crafted for comfort and class, ensuring you stand out with grace at every occasion.",
+    number:"13/02"
+},
+{
+    img:"../img/blog/bl4.jpg",
+    name:"Urban Cool" ,
+    des:" Streetwear-inspired pieces that combine casual comfort with an edge of contemporary flair.",
+    number:"13/03"
+},
+{
+    img:"../img/blog/bl5.jpg",
+    name:"Playful and Vibrant",
+    des:" Add a pop of color and pattern to your wardrobe with fun, spirited designs that spark joy.",
+    number:"13/04"
+},
+{
+    img:"../img/blog/bl6.jpg",
+    name:"Power Dressing",
+    des:" Sleek, tailored pieces designed to boost confidence and make you feel unstoppable in any setting.",
+    number:"13/05"
+},{
+    img:"../img/blog/bl7.jpg",
+    name:"Fusion Flair",
+    des:"A perfect mix of traditional and contemporary styles, showcasing the best of cultural-inspired modern fashion.",
+    number:"13/06"
+}
+];
