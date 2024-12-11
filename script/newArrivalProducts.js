41425b141a555c1eb3f63f0879025ea45efe7b9e
shinesha_W <shinesha1112@gmail.com>
2024-12-11 16:28:09 +0530

    new arrival products

---
+++ script/newArrivalProducts.js
@@ -0,0 +1,26 @@
+import { newArrival } from "./newproducts.js";
+const productContainer = document.getElementById("pro-container2");
+
+
+    if (productContainer) {
+        newArrival.forEach(product => {
+            const starsHTML = Array(product.stars)
+                .fill('<i class="fas fa-star"></i>')
+                .join('');
+
+            productContainer.innerHTML += `
+                <div class="pro">
+                    <img src="${product.img}" alt="" class="pro-img">
+                    <div class="des">
+                        <span>${product.brand}</span>
+                        <h5>${product.description}</h5>
+                        <div class="star">${starsHTML}</div>
+                        <h4>${product.price}</h4>
+                    </div>
+                    <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
+                </div>
+            `;
+        });
+    } else {
+        console.error("Element with id 'product-container' not found.");
+    }
