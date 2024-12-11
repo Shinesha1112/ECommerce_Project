aca6dea06784e499591e0e29d60455863592d123
shinesha_W <shinesha1112@gmail.com>
2024-12-11 16:31:28 +0530

    products details

---
+++ script/products.js
@@ -0,0 +1,60 @@
+export const products = [
+    {
+        img: "../img/products/f1.jpg",
+        brand: "HI-FASHION",
+        description: "Casual Floral Shirt For Women",
+        price: "Rs. 500",
+        stars: 4
+    },
+    {
+        img: "../img/products/f2.jpg",
+        brand: "TRENDY",
+        description: " Casual Floral Shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        img: "../img/products/f3.jpg",
+        brand: "PARK",
+        description: "DressBerry Blooming Floral Shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        img: "../img/products/f4.jpg",
+        brand: "INDO",
+        description: "Stylish Casual shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        img: "../img/products/f5.jpg",
+        brand: "SCOTT",
+        description: "Casual Stylish Printed Regular Fit",
+        price: "Rs. 349",
+        stars: 4
+    },
+    {
+        img: "../img/products/f6.jpg",
+        brand: "PERFECT",
+        description: "Women's Solid Oversized Fit Shirt",
+        price: "Rs. 819",
+        stars: 5
+    },
+    {
+        img: "../img/products/f7.jpg",
+        brand: "BROOK",
+        description: "Floral Pant",
+        price: "Rs. 399",
+        stars: 4
+    },
+    {
+        img: "../img/products/f8.jpg",
+        brand: "PREMIUM",
+        description: "Stylish Casual Top",
+        price: "Rs. 699",
+        stars: 5
+    }
+  
+
+];
