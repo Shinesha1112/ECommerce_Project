bfcf13f03f5520c950b31cdaff3c80ddf65f1677
shinesha_W <shinesha1112@gmail.com>
2024-12-11 16:34:13 +0530

    shop products details

---
+++ script/shopProduct.js
@@ -0,0 +1,131 @@
+export const shopProducts = [
+    {   
+        id:1,
+        img: "../img/products/f1.jpg",
+        brand: "HI-FASHION",
+        description: "Casual Floral Shirt For Women",
+        price: "Rs. 500",
+        stars: 4
+    },
+    {
+        id:2,
+        img: "../img/products/n1.jpg",
+        brand: "LAND",
+        description: "Men's Solid Regular Fit Shirt",
+        price: "Rs. 499",
+        stars: 3
+    },
+    {
+        id:3,
+        img: "../img/products/f2.jpg",
+        brand: "TRENDY",
+        description: " Casual Floral Shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        id:4,
+        img: "../img/products/n2.jpg",
+        brand: "FASHION",
+        description: "Casual Shirts For Men",
+        price: "Rs. 456",
+        stars: 4
+    },
+    {
+        id:5,
+        img: "../img/products/f3.jpg",
+        brand: "PARK",
+        description: "DressBerry Blooming Floral Shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        id:6,
+        img: "../img/products/n3.jpg",
+        brand: "MARK",
+        description: "Men's Cotton Blend Regular Fit Shirt",
+        price: "Rs. 379",
+        stars: 4
+    },
+    {
+        id:7,
+        img: "../img/products/f4.jpg",
+        brand: "INDO",
+        description: "Stylish Casual shirt",
+        price: "Rs. 799",
+        stars: 5
+    },
+    {
+        id:8,
+        img: "../img/products/n4.jpg",
+        brand: "PARK",
+        description: "Cotton Casual Printed Shirt",
+        price: "Rs. 399",
+        stars: 3
+    },
+    {
+        id:9,
+        img: "../img/products/f5.jpg",
+        brand: "SCOTT",
+        description: "Casual Stylish Printed Regular Fit",
+        price: "Rs. 349",
+        stars: 4
+    },
+    {
+        id:10,
+        img: "../img/products/n5.jpg",
+        brand: "PERFECT",
+        description: "Casual Shirt",
+        price: "Rs. 599",
+        stars: 3
+    },
+    {
+        id:11,
+        img: "../img/products/f6.jpg",
+        brand: "PERFECT",
+        description: "Women's Solid Oversized Fit Shirt",
+        price: "Rs. 819",
+        stars: 5
+    },
+    {
+        id:12,
+        img: "../img/products/n6.jpg",
+        brand: "BROOK",
+        description: "Men's Regular Cotton Shorts",
+        price: "Rs. 299",
+        stars: 3
+    },
+    {
+        id:13,
+        img: "../img/products/f7.jpg",
+        brand: "BROOK",
+        description: "Floral Pant",
+        price: "Rs. 399",
+        stars: 4
+    },
+    {
+        id:14,
+        img: "../img/products/n8.jpg",
+        brand: "INDO",
+        description: "Men's Regular Fit Shirt",
+        price: "Rs. 569",
+        stars: 4
+    },
+    { id:15,
+        img: "../img/products/f8.jpg",
+        brand: "PREMIUM",
+        description: "Stylish Casual Top",
+        price: "Rs. 699",
+        stars: 5
+    },
+    {
+        id:16,
+        img: "../img/products/n7.jpg",
+        brand: "SCOTT",
+        description: "casual Shirt With Full Sleeve",
+        price: "Rs. 499",
+        stars: 5
+    }
+  
+
+];
