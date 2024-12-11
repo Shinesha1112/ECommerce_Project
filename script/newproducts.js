60b3bb75bdda41638b56321d54a1ff80414adedb
shinesha_W <shinesha1112@gmail.com>
2024-12-11 16:30:17 +0530

    new products details

---
+++ script/newproducts.js
@@ -0,0 +1,59 @@
+export const newArrival=[
+    {
+        img: "../img/products/n1.jpg",
+        brand: "LAND",
+        description: "Men's Solid Regular Fit Shirt",
+        price: "Rs. 499",
+        stars: 3
+    },
+    {
+        img: "../img/products/n2.jpg",
+        brand: "FASHION",
+        description: "Casual Shirts For Men",
+        price: "Rs. 456",
+        stars: 4
+    },
+    {
+        img: "../img/products/n3.jpg",
+        brand: "MARK",
+        description: "Men's Cotton Blend Regular Fit Shirt",
+        price: "Rs. 379",
+        stars: 5
+    },
+    {
+        img: "../img/products/n4.jpg",
+        brand: "PARK",
+        description: "Cotton Casual Printed Shirt",
+        price: "Rs. 399",
+        stars: 3
+    },
+    {
+        img: "../img/products/n5.jpg",
+        brand: "PERFECT",
+        description: "Casual Shirt",
+        price: "Rs. 599",
+        stars: 5
+    },
+    {
+        img: "../img/products/n6.jpg",
+        brand: "BROOK",
+        description: "Men's Regular Cotton Shorts",
+        price: "Rs. 299",
+        stars: 5
+    },
+    {
+        img: "../img/products/n7.jpg",
+        brand: "SCOTT",
+        description: "casual Shirt With Full Sleeve",
+        price: "Rs. 499",
+        stars: 5
+    },
+   
+    {
+        img: "../img/products/n8.jpg",
+        brand: "INDO",
+        description: "Men's Regular Fit Shirt",
+        price: "Rs. 569",
+        stars: 5
+    }
+];
